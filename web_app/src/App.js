728e12b5d7ace9e5fbf3e4bd7c67ec425cf51fe2
Martin Vidjeskog <martin.vidjeskog@hotmail.com>
2019-11-05 13:47:16 +0200

    Init setup

---
+++ web_app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Helmet } from 'react-helmet';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Helmet><title>Mobiilishakki</title></Helmet>
+      <h1>Mobiilishakki</h1>
+    </div>
+  );
+}
+
+export default App;
