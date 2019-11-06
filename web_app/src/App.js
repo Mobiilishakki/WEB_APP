79d0b346862fd37d67f548202f53f36370193182
Martin Vidjeskog <martin.vidjeskog@hotmail.com>
2019-11-06 12:30:11 +0200

    game.js + removed Helmet + basic input and output

--- web_app/src/App.js
+++ web_app/src/App.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import { Helmet } from 'react-helmet';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
+const Button = ({ text }) => {
+  return (
+    <div>
+      <button>{text}</button>
+    </div>
+  )
+}
+
+const TextArea = ({ content, rows, cols, readOnly }) => {
+  return (
+    <div>
+      <textarea value={content} rows={rows} cols={cols} readOnly={readOnly} />
+    </div>
+  )
+}
+
 const App = () => {
+  const [fenNotation, setFenNotation] = useState('')
+
   return (
     <div className="App">
-      <Helmet><title>Mobiilishakki</title></Helmet>
+
       <h1>Mobiilishakki</h1>
+
+      <Button text="Connect"></Button>
+
+      <br />
+      <TextArea content={fenNotation} rows={10} cols={50} readOnly={true} />
     </div>
   );
 }
