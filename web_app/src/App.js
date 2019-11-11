9ed0287ab9a25ada209b5a3592293c0e80ad259a
Martin Vidjeskog <martin.vidjeskog@hotmail.com>
2019-11-11 14:54:32 +0200

    Chessboard component and fen parsing

--- web_app/src/App.js
+++ web_app/src/App.js
@@ -19,33 +19,30 @@ const FENTextArea = ({ content, rows, cols, readOnly }) => {
   )
 }
 
-const LastMoveText = ({text}) => {
-  return (
-    <div>
-      <p><strong>{text}</strong></p>
-    </div>
-  )
-}
-
 const App = () => {
-  const [fenNotation, setFenNotation] = useState('')  // fen notation of game state
-  const [lastMove, setLastMove] = useState('')        // last move of current game        
-  const [gameOn, setGameOn] = useState(false)         // is the game on or not
+  const [fenNotation, setFenNotation] = useState('')      // fen notation of game state      
+  const [gameOn, setGameOn] = useState(false)             // is the game on or not
+  const [playerColor, setPlayerColor] = useState('black') // player color (white or black)
 
   const changeGameState = () => { // set game to on or off
     setGameOn(!gameOn)
+    // CHANGE THIS !!!
+    setFenNotation(gameOn === true ? "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR" : '')
+  }
+
+  const changePlayerColor = () => { // change player color to white or black
+    setPlayerColor(playerColor === "black" ? "white" : "black")
   }
 
   return (
     <div className="App">
       <h1>Mobiilishakki</h1>
-      <Button onClick={changeGameState} text={gameOn ? 'Disconnect' : 'Connect'}></Button>
-      <h3>FEN-notaatio</h3>
-      <FENTextArea content="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1" rows={5} cols={50} readOnly={true} />
-      <h3>Viimeisin siirto</h3>
-      <LastMoveText text="Valkoinen Ratsu: From B1 To A3"/>
-      <h3>Pelitilanne</h3>
-      <Chessboard />
+      <Button onClick={changeGameState} text={gameOn ? 'Disconnect' : 'Connect'} />
+      <h3>FEN</h3>
+      <FENTextArea content="rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR" rows={5} cols={50} readOnly={true} />
+      <h3>Gamestate</h3>
+      <Button onClick={changePlayerColor} text={"Change player view"} />
+      <Chessboard playerColor={playerColor} fen={fenNotation} />
     </div>
   );
 }
--- web_app/src/components/Chessboard.js
+++ web_app/src/components/Chessboard.js
@@ -1,16 +1,41 @@
 import React from 'react'
 import './Chessboard.css'
 
+// array containing pieces information from a1 to h8
+const PIECES = Array(64).fill('')
+
+// function takes fen notation as input and
+// fills the pieces array with pieces information.
+const parseFenNotation = (fen) => {
+  let square = 0
+  for (let i = 0; i < fen.length; i++) {
+    if (square >= 64) {
+      break
+    }
+    let c = fen.charAt(i)
+    if (c === '/') { // next line
+      continue
+    } else if (c >= '0' && c <= '9') {  // number of empty squares
+      const blanks = parseInt(c)
+      square = square + blanks - 1
+    } else { // actual chess piece
+      PIECES[square] = c
+    }
+    square = square + 1
+  }
+}
+
 // single chessboard square. 
-// takes color of the square as parameter
-const ChessboardSquare = ({ color }) => {
+// takes row number, column number and color of the square as parameters
+const ChessboardSquare = ({ row, col, color }) => {
+  const piece = (row - 1) * 8 + col - 1 // index of the piece
   if (color === 'white') {
     return (
-      <td className="light"></td>
+      <td className="light">{PIECES[piece]}</td>
     )
   }
   return (
-    <td className="dark"></td>
+    <td className="dark">{PIECES[piece]}</td>
   )
 }
 
@@ -22,14 +47,14 @@ const ChessboardRow = ({ rowNum, color }) => {
   return (
     <tr>
       <th>{rowNum}</th>
-      <ChessboardSquare color={color} />
-      <ChessboardSquare color={otherColor} />
-      <ChessboardSquare color={color} />
-      <ChessboardSquare color={otherColor} />
-      <ChessboardSquare color={color} />
-      <ChessboardSquare color={otherColor} />
-      <ChessboardSquare color={color} />
-      <ChessboardSquare color={otherColor} />
+      <ChessboardSquare row={rowNum} col={1} color={color} />
+      <ChessboardSquare row={rowNum} col={2} color={otherColor} />
+      <ChessboardSquare row={rowNum} col={3} color={color} />
+      <ChessboardSquare row={rowNum} col={4} color={otherColor} />
+      <ChessboardSquare row={rowNum} col={5} color={color} />
+      <ChessboardSquare row={rowNum} col={6} color={otherColor} />
+      <ChessboardSquare row={rowNum} col={7} color={color} />
+      <ChessboardSquare row={rowNum} col={8} color={otherColor} />
     </tr>
   )
 }
@@ -52,19 +77,23 @@ const TopAxis = () => {
 }
 
 // chessboard component
-const Chessboard = () => {
+// takes fen notation and player color (white, black) as parameters
+const Chessboard = ({ playerColor, fen }) => {
+  parseFenNotation(fen) // maybe use-effect?
+  const rownums = playerColor === "black" ? [1, 2, 3, 4, 5, 6, 7, 8] : [8, 7, 6, 5, 4, 3, 2, 1]
+  const enemyColor = playerColor === "black" ? "white" : "black"
   return (
     <table>
       <tbody>
         <TopAxis />
-        <ChessboardRow rowNum={1} color="black" />
-        <ChessboardRow rowNum={2} color="white" />
-        <ChessboardRow rowNum={3} color="black" />
-        <ChessboardRow rowNum={4} color="white" />
-        <ChessboardRow rowNum={5} color="black" />
-        <ChessboardRow rowNum={6} color="white" />
-        <ChessboardRow rowNum={7} color="black" />
-        <ChessboardRow rowNum={8} color="white" />
+        <ChessboardRow rowNum={rownums[0]} color={playerColor} />
+        <ChessboardRow rowNum={rownums[1]} color={enemyColor} />
+        <ChessboardRow rowNum={rownums[2]} color={playerColor} />
+        <ChessboardRow rowNum={rownums[3]} color={enemyColor} />
+        <ChessboardRow rowNum={rownums[4]} color={playerColor} />
+        <ChessboardRow rowNum={rownums[5]} color={enemyColor} />
+        <ChessboardRow rowNum={rownums[6]} color={playerColor} />
+        <ChessboardRow rowNum={rownums[7]} color={enemyColor} />
       </tbody>
     </table>
   )
